7e4c60dbf7a0906cafa6b87563166f07d553296b
NoobInCoding <srzrezamms@gmail.com>
2025-09-04 20:25:15 +0330

    Add project files.

---
+++ SilksongDecryptor/Encryption.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SilksongDecryptor
+{
+    public static class Encryption
+    {
+        private static readonly byte[] _keyArray = Encoding.UTF8.GetBytes("UKu52ePUBwetZ9wNX88o54dnfKRu0T1l");
+        private static readonly Aes _aes = Aes.Create();
+
+        static Encryption()
+        {
+            _aes.Key = _keyArray;
+            _aes.Mode = CipherMode.ECB;
+            _aes.Padding = PaddingMode.PKCS7;
+        }
+
+        public static byte[] Encrypt(byte[] decryptedBytes)
+        {
+            return _aes.EncryptEcb(decryptedBytes, PaddingMode.PKCS7);
+        }
+
+        public static byte[] Decrypt(byte[] encryptedBytes)
+        {
+            return _aes.DecryptEcb(encryptedBytes, PaddingMode.PKCS7);
+        }
+
+        public static string Encrypt(string unencryptedString)
+        {
+            byte[] array = Encrypt(Encoding.UTF8.GetBytes(unencryptedString));
+            return Convert.ToBase64String(array, 0, array.Length);
+        }
+
+        public static string Decrypt(string encryptedString)
+        {
+            byte[] bytes = Decrypt(Convert.FromBase64String(encryptedString));
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+
+}
---
+++ SilksongDecryptor/Program.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+
+namespace SilksongDecryptor
+{
+    internal class Program
+    {
+        public class TextAsset
+        {
+            public required string m_Name;
+            public required string m_Script;
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length > 2)
+            {
+                Console.WriteLine("Usage: SilksongDecryptor -decrypt|-encrypt <folder_path>");
+                return;
+            }
+            if (args[0] == "-decrypt")
+            {
+                string outpath = Path.Combine(Path.GetDirectoryName(args[1]), Path.GetFileName(args[1]) + "_Decrypted");
+                Directory.CreateDirectory(outpath);
+                foreach (string path in Directory.GetFiles(args[1], "*.json", SearchOption.AllDirectories))
+                {
+                    string json = File.ReadAllText(path);
+                    TextAsset asset = JsonConvert.DeserializeObject<TextAsset>(json) ?? throw new Exception("Json deresialize failed!");
+                    string decrypted = Encryption.Decrypt(asset.m_Script);
+                    Console.WriteLine($"Decrypted {asset.m_Name}");
+                    decrypted = asset.m_Name + "\n" + decrypted;
+                    File.WriteAllText(Path.Combine(outpath, Path.ChangeExtension(Path.GetFileName(path), ".txt")), decrypted);
+                }
+            }else if(args[0] == "-encrypt")
+            {
+                string outpath = Path.Combine(Path.GetDirectoryName(args[1]), Path.GetFileName(args[1]) + "_Encrypted");
+                Directory.CreateDirectory(outpath);
+                foreach (string path in Directory.GetFiles(args[1], "*.txt", SearchOption.AllDirectories))
+                {
+                    string[] lines = File.ReadAllLines(path);
+                    if (lines.Length < 2) continue;
+                    string name = lines[0];
+                    string script = string.Join("\n", lines.Skip(1));
+                    string encrypted = Encryption.Encrypt(script);
+                    TextAsset asset = new()
+                    {
+                        m_Name = name,
+                        m_Script = encrypted
+                    };
+                    string json = JsonConvert.SerializeObject(asset, Formatting.Indented);
+                    Console.WriteLine($"Encrypted {asset.m_Name}");
+                    File.WriteAllText(Path.Combine(outpath, Path.ChangeExtension(Path.GetFileName(path), ".json")), json);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: SilksongDecryptor -decrypt|-encrypt <folder_path>");
+            }
+        }
+    }
+}
